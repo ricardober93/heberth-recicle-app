134124b265547161fcd1aac93d527c017c13366a
Ricardo Bermudez Tellez <39892056+ricardober93@users.noreply.github.com>
2025-07-23 12:44:08 -0500

    build(drizzle): update config to use postgresql dialect

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,8 +6,8 @@ dotenv.config();
 export default {
   schema: './src/models/schema.ts',
   out: './drizzle',
-  driver: 'pg',
+    dialect: 'postgresql',
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!,
   },
 } satisfies Config;
